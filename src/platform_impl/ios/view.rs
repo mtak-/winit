5e0d457c7a208856839f79ef85067af68a673602
tyler <tyler@brainiumstudios.com>
2018-12-03 15:44:33 -0800

    call super on these methods. since we allow custom subclasses of uiview

--- src/platform_impl/ios/view.rs
+++ src/platform_impl/ios/view.rs
@@ -142,14 +142,13 @@ unsafe fn get_view_controller_class() -> &'static Class {
 unsafe fn get_window_class() -> &'static Class {
     static mut CLASS: Option<&'static Class> = None;
     if CLASS.is_none() {
-        let uiwindow_class = class!(UIWindow);
-
         extern fn become_key_window(object: &Object, _: Sel) {
             unsafe {
                 AppState::handle_nonuser_event(Event::WindowEvent {
                     window_id: RootWindowId(object.into()),
                     event: WindowEvent::Focused(true),
-                })
+                });
+                let () = msg_send![super(object, class!(UIWindow)), becomeKeyWindow];
             }
         }
 
@@ -158,7 +157,8 @@ unsafe fn get_window_class() -> &'static Class {
                 AppState::handle_nonuser_event(Event::WindowEvent {
                     window_id: RootWindowId(object.into()),
                     event: WindowEvent::Focused(false),
-                })
+                });
+                let () = msg_send![super(object, class!(UIWindow)), resignKeyWindow];
             }
         }
 
